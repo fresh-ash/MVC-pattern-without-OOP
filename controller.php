fee8b51b876923fa736266ff8e81448bf3ab0905
fresh-ash <sergei081@gmail.com>
2016-08-05 11:14:38 +0300

    Create controller.php

---
+++ controller.php
@@ -0,0 +1,10 @@
+# MVC-pattern-without-OOP
+
+<?php
+include_once('model.php');
+include_once('view.php');
+
+showPHPPage(getContent(), getTopMenuItems());
+#А можно запилить showDesktop(), к примеру!
+
+?>
